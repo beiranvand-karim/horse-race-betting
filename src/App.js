93df72fdf973966fdb9e31b8106836843276823a
Karim Beiranvand <beiranvand.karim@gmail.com>
2022-11-28 15:06:37 +0100

    fonts and global styles (#6)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { GlobalStyles } from './styles';
+
 const App = () => {
-  return <div>E-commerce</div>;
+  return (
+    <div>
+      <GlobalStyles />
+      <span>E-commerce</span>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+  
+  html, body, * {
+    font-family: Mulish, "Helvetica Neue", Helvetica, Roboto, sans-serif;
+    box-sizing: border-box;
+  }
+`;
