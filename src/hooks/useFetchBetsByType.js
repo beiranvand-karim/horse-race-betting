849ab53cd523c2fbde8b624ce12297c029e3ca47
Karim Beiranvand <beiranvand.karim@gmail.com>
2022-11-29 14:16:43 +0100

    fetch bets and games hook implementations (#10)
    (cherry picked from commit 842876160851e1d988251fa2d359637144c9fe7f)

---
+++ src/hooks/useFetchBetsByType.js
@@ -0,0 +1,37 @@
+import { useContext, useState } from 'react';
+import axios from 'axios';
+
+import { EnvironmentVariables } from '../environmentVariables';
+
+export const useFetchBetsByType = () => {
+  const [bets, setBets] = useState(null);
+  const [error, setError] = useState(null);
+  const [betsLoading, setBetsLoading] = useState(false);
+  const { DATA_SOURCE_NETWORK_URL } = useContext(EnvironmentVariables);
+
+  const fetchBets = async (id) => {
+    try {
+      if (!DATA_SOURCE_NETWORK_URL) {
+        return setError(new Error('Network data source URL is missing.'));
+      }
+      setBetsLoading(true);
+      const response = await axios.get(
+        `${DATA_SOURCE_NETWORK_URL}/services/racinginfo/v1/api/products/${id}`
+      );
+      setBets(response.data);
+    } catch (capturedError) {
+      /**
+       * log the error to sentry, kibana ...
+       * for example:
+       * log({type: 'error', message: 'failed to fetch bets', data: {capturedError, userId})
+       */
+      setError(capturedError);
+    } finally {
+      setBetsLoading(false);
+    }
+  };
+
+  const betsError = error && 'Failed to fetch bets.';
+
+  return { bets, betsError, betsLoading, fetchBets };
+};
---
+++ src/hooks/useFetchGameById.js
@@ -0,0 +1,41 @@
+import { useContext, useEffect, useState } from 'react';
+import { EnvironmentVariables } from '../environmentVariables';
+import axios from 'axios';
+
+export const useFetchGameById = (id) => {
+  const [game, setGame] = useState(null);
+  const [error, setError] = useState(null);
+  const [gameLoading, setGameLoading] = useState(false);
+  const { DATA_SOURCE_NETWORK_URL } = useContext(EnvironmentVariables);
+
+  const fetchGame = async () => {
+    try {
+      // this appears no both hooks, can be refactored away
+      if (!DATA_SOURCE_NETWORK_URL) {
+        return setError(new Error('Network data source URL is missing.'));
+      }
+      setGameLoading(true);
+      const response = await axios.get(
+        `${DATA_SOURCE_NETWORK_URL}/services/racinginfo/v1/api/games/${id}`
+      );
+      setGame(response.data);
+    } catch (capturedError) {
+      /**
+       * log the error to sentry, kibana ...
+       * for example:
+       * log({type: 'error', message: 'failed to fetch game', data: {capturedError, userId, gameId: id})
+       */
+      setError(capturedError);
+    } finally {
+      setGameLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchGame();
+  }, []);
+
+  const gameError = error && 'Failed to fetch the game.';
+
+  return { game, gameError, gameLoading };
+};
